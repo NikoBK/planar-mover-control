729b3adb4c174ed029626d7e08d502836b1d0a7d
NikoBK <112914618+NikoBK@users.noreply.github.com>
2025-07-11 15:17:01 +0200

    pmc connectivity test

--- src/planar-mover-control/Program.cs
+++ src/planar-mover-control/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PMCLIB;
+
+namespace PlanarMoverControl
+{
+    class Program
+    {
+        private static SystemCommands _sysCmds = new SystemCommands();
+
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            PMCConnect();
+        }
+
+        private static bool PMCConnect() {
+            Console.WriteLine("Trying to connect to the pmc...");
+            bool connected = _sysCmds.AutoSearchAndConnectToPMC();
+            if (!connected) {
+                Console.WriteLine("Failed to connect to the pmc");
+                return false;
+            }
+            return true;
+        }
+    }
+}
